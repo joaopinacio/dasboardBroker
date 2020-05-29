7ccabc8dcea8494f9a715c4ea18e7a0d3ce05fbd
joaopinacio <joaopedro.triun@gmail.com>
2020-05-29 00:45:35 -0300

    Implementação de Botoes e Sensor de Distancia

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var bar;
 $( document ).ready(function() {
 
+    $('#toggle').parent().attr('content-off','FECHADA');
+    $('#toggle').parent().attr('content-on','ABERTA');
+    $('#toggleTwo').parent().attr('content-off','OFF');
+    $('#toggleTwo').parent().attr('content-on','ON');
+
+
     bar = new ProgressBar.SemiCircle('#progressBar', {
         strokeWidth: 6,
         color: '#FFEA82',
@@ -69,36 +75,36 @@ $( document ).ready(function() {
           }
         }
     });
+    
+    $('#distValue').countTo({
+        from: +$('#distValue').html(),
+        to: 100,
+        speed: 2000,
+        refreshInterval: 60,
+        onComplete: function(value) {}
+    });
 
-    // var bar = new ProgressBar.Circle('#progressBar', {
-    //     color: '#aaa',
-    //     strokeWidth: 10,
-    //     trailWidth: 5,
-    //     easing: 'easeInOut',
-    //     duration: 2500,
-    //     text: {
-    //       autoStyleContainer: false
-    //     },
-    //     from: { color: '#aaa', width: 3 },
-    //     to: { color: '#333', width: 8 },
-    //     step: function(state, circle) {
-    //       circle.path.setAttribute('stroke', state.color);
-    //       circle.path.setAttribute('stroke-width', state.width);
-      
-    //       var value = Math.round(circle.value() * 20);
-    //       if (value === 0) {
-    //         circle.setText('');
-    //       } else {
-    //         circle.setText(value);
-    //       }
-      
-    //     }
-    // });
-    //   bar.text.style.fontFamily = '"Lato", Helvetica Neue, Arial, Helvetica, sans-serif';
-    //   bar.text.style.fontSize = '1.5rem';
-    //   bar.text.style.fontWeight = '400';
-    //   bar.text.style.color = '#1b1c1d';
-      
-    //   bar.animate(0.5);  // Number from 0.0 to 1.0
+    $('#toggleDoor').click(function (e){
+      $('#toggleDoor').parent().parent().toggleClass('on');
+    });
 
+    $('#toggleAir').click(function (e){
+      $('#toggleAir').parent().parent().toggleClass('on');
+    });
+    
+    $('#toggleLum').click(function (e){
+      $('#toggleLum').parent().parent().toggleClass('on');
+    });
+
+    $('#toggleMult').click(function (e){
+      $('#toggleMult').parent().parent().toggleClass('on');
+    });
+
+    $('#toggleDoorr').click(function (e){
+      $('#toggleDoorr').parent().parent().toggleClass('on');
+    });
+
+    $('#togglePresence').click(function (e){
+      $('#togglePresence').parent().parent().toggleClass('on');
+    });
 });
